a329d2d352ec51d354f7b33146b5cf1c4da0afaa
Manuel <mpduran@fi.uba.ar>
2025-03-14 22:55:26 +0100

    remove os

--- shots_analysis_app.py
+++ shots_analysis_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import os
 
 # Título de la aplicación
 st.title("Shot Analysis")
@@ -11,13 +10,4 @@ st.subheader("Estudio Histórico de Tiros y Estadísticas")
 # Mensaje de bienvenida
 st.write("Bienvenido a mi aplicación. Aquí puedes agregar más funcionalidades.")
 
-# Importar archivos
-def excel_total(pathCarpeta):
-    files = [f for f in os.listdir(pathCarpeta) if f.endswith('.xlsx') or f.endswith('.xls')]
-    dfs = [pd.read_excel(os.path.join(pathCarpeta, f), sheet_name='Eventos') for f in files]
-    
-    return pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
-
-league = 'Spain La Liga'
-folder_path = f'/Partidos/{league}/24-25'
-df = excel_total(f'{folder_path}')
+df = pd.read_csv('xgot_model_version_17_02.csv')
