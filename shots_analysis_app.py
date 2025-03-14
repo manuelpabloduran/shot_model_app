142cd7117854ce7ac3fcf6d7c31c5db4008079c7
Manuel <mpduran@fi.uba.ar>
2025-03-14 22:50:01 +0100

    add reading files

--- shots_analysis_app.py
+++ shots_analysis_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Título de la aplicación
 st.title("Shot Analysis")
@@ -8,3 +9,14 @@ st.subheader("Estudio Histórico de Tiros y Estadísticas")
 
 # Mensaje de bienvenida
 st.write("Bienvenido a mi aplicación. Aquí puedes agregar más funcionalidades.")
+
+# Importar archivos
+def excel_total(pathCarpeta):
+    files = [f for f in os.listdir(pathCarpeta) if f.endswith('.xlsx') or f.endswith('.xls')]
+    dfs = [pd.read_excel(os.path.join(pathCarpeta, f), sheet_name='Eventos') for f in tqdm(files)]
+    
+    return pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
+
+league = 'Spain La Liga'
+folder_path = f'/Partidos/{league}/24-25'
+df = excel_total(f'{folder_path}')
