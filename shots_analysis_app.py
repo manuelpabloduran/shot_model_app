394d5545f950e30a0c8a2067f2bb3e64e4d570ff
Manuel <mpduran@fi.uba.ar>
2025-03-14 20:58:45 +0100

    add main app python file and requirements txt

---
+++ shots_analysis_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Shot Analysis")
+
+# Subtítulo
+st.subheader("Estudio Histórico de Tiros y Estadísticas")
+
+# Mensaje de bienvenida
+st.write("Bienvenido a mi aplicación. Aquí puedes agregar más funcionalidades.")
