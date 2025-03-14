62f0303fd392c1932d6dd9651f666e159b517758
Manuel <mpduran@fi.uba.ar>
2025-03-14 23:01:48 +0100

    Add example chart

--- shots_analysis_app.py
+++ shots_analysis_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib as plt
 
 # Título de la aplicación
 st.title("Shot Analysis")
@@ -11,3 +12,13 @@ st.subheader("Estudio Histórico de Tiros y Estadísticas")
 st.write("Bienvenido a mi aplicación. Aquí puedes agregar más funcionalidades.")
 
 df = pd.read_csv('xgot_model_version_17_02.csv')
+
+# Crear gráfico de dispersión
+fig, ax = plt.subplots()
+ax.scatter(df['x'], df['y'], alpha=0.5)
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_title("Gráfico de dispersión de X vs Y")
+
+# Mostrar gráfico en Streamlit
+st.pyplot(fig)
