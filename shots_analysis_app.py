af778664ba647b56e6f810b5460062e918c0765d
Manuel <mpduran@fi.uba.ar>
2025-03-14 23:08:49 +0100

    Add goalkeeper analysis

--- shots_analysis_app.py
+++ shots_analysis_app.py
@@ -5,20 +5,31 @@ import matplotlib.pyplot as plt
 # Título de la aplicación
 st.title("Shot Analysis")
 
-# Subtítulo
-st.subheader("Estudio Histórico de Tiros y Estadísticas")
-
-# Mensaje de bienvenida
-st.write("Bienvenido a mi aplicación. Aquí puedes agregar más funcionalidades.")
-
+# Cargar datos
 df = pd.read_csv('xgot_model_version_17_02.csv')
 
-# Crear gráfico de dispersión
-fig, ax = plt.subplots()
-ax.scatter(df['x'], df['y'], alpha=0.5)
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_title("Gráfico de dispersión de X vs Y")
+# Crear pestañas
+tab1, tab2 = st.tabs(["GoalKeeper Analysis", "Historical Shot Analysis"])
 
-# Mostrar gráfico en Streamlit
-st.pyplot(fig)
+with tab1:
+    st.subheader("GoalKeeper Analysis")
+    
+    # Filtro de selección de portero
+    selected_gk = st.selectbox("Selección de Portero para el análisis", df['NaPlayer_gk'].unique())
+    
+    # Filtrar datos por portero seleccionado
+    df_filtered = df[df['NaPlayer_gk'] == selected_gk]
+    
+    # Crear gráfico de dispersión
+    fig, ax = plt.subplots()
+    ax.scatter(df_filtered['x'], df_filtered['y'], alpha=0.5)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_title(f"Gráfico de dispersión de X vs Y para {selected_gk}")
+    
+    # Mostrar gráfico en Streamlit
+    st.pyplot(fig)
+
+with tab2:
+    st.subheader("Historical Shot Analysis")
+    st.write("Aquí se pueden agregar más análisis históricos.")
