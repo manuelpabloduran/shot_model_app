d817def2a593c98de0e5bfbf285c881132db2bda
Manuel <mpduran@fi.uba.ar>
2025-03-14 22:53:33 +0100

    remove tqdm from script

--- shots_analysis_app.py
+++ shots_analysis_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import os
-from tqdm import tqdm
 
 # Título de la aplicación
 st.title("Shot Analysis")
@@ -15,7 +14,7 @@ st.write("Bienvenido a mi aplicación. Aquí puedes agregar más funcionalidades
 # Importar archivos
 def excel_total(pathCarpeta):
     files = [f for f in os.listdir(pathCarpeta) if f.endswith('.xlsx') or f.endswith('.xls')]
-    dfs = [pd.read_excel(os.path.join(pathCarpeta, f), sheet_name='Eventos') for f in tqdm(files)]
+    dfs = [pd.read_excel(os.path.join(pathCarpeta, f), sheet_name='Eventos') for f in files]
     
     return pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
 
