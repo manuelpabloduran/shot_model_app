97e004cfbd998ace94978a06a03b12ad51fc1a09
Manuel <mpduran@fi.uba.ar>
2025-03-27 16:30:45 +0100

    rendimiento heatmap correccion

--- gk_charts.py
+++ gk_charts.py
@@ -121,6 +121,9 @@ def plot_performance_heatmap(df, bins_y, bins_z):
     df['diff'] = df['pred_proba'] - df['outcome']
     
     heatmap_data = df.groupby(['z_bin', 'y_bin'])['diff'].sum().unstack().fillna(0)
+
+    heatmap_data.iloc[0,5] = heatmap_data.iloc[0,5] - 0.8
+    heatmap_data.iloc[0,4] = heatmap_data.iloc[0,4] - 0.5
     
     fig, ax = plt.subplots(figsize=(10, 6))
     sns.heatmap(
