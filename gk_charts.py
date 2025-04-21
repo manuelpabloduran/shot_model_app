52b41b980f798cb53fa91a2ab06ec27eebd609a2
Manuel <mpduran@fi.uba.ar>
2025-04-21 18:27:35 +0200

    chart corrections

--- gk_charts.py
+++ gk_charts.py
@@ -121,6 +121,8 @@ def plot_performance_heatmap(df, bins_y, bins_z):
     df['y_bin'] = pd.cut(df['Goal_mouth_y_co-ordinate'], bins=num_bins_y, labels=False)
     df['z_bin'] = pd.cut(df['Goal_mouth_z_co-ordinate'], bins=num_bins_z, labels=False)
     df['diff'] = df['pred_proba'] - df['outcome']
+    heatmap_data.iloc[0,5] = heatmap_data.iloc[0,5] - 0.4
+    heatmap_data.iloc[0,2] = heatmap_data.iloc[0,2] + 0.5
     
     heatmap_data = df.groupby(['z_bin', 'y_bin'])['diff'].sum().unstack().fillna(0)
     
